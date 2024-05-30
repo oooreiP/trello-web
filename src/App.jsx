57f0101f18ec3b7794e11e23ddd8cf323daf68b5
oooreiP <vietman151@gmail.com>
2024-05-30 10:56:51 +0700

    config-theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
-import DeleteIcon from '@mui/icons-material/Delete';
-import HomeIcon from '@mui/icons-material/Home';
-import { pink } from '@mui/material/colors';
-import Button from '@mui/material/Button';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import DeleteIcon from '@mui/icons-material/Delete'
+import HomeIcon from '@mui/icons-material/Home'
+import { pink } from '@mui/material/colors'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 
 
 function App() {
   return (
     <>
-    <div>Viet Man</div>
+      <div>Viet Man</div>
+      <Typography variant="body2" color="text.secondary">Test Typo</Typography>
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
+      <Button variant="contained" color="success">Contained</Button>
       <Button variant="outlined">Outlined</Button>
       <AccessAlarmIcon/>
       <ThreeDRotation/>
